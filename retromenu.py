548cef2a177d8b951ee06db8a498f2dbee97eba9
Nack323 <39598790+Nack323@users.noreply.github.com>
2020-08-12 23:38:05 -0500

    Add files via upload

---
+++ retromenu.py
@@ -0,0 +1,38 @@
+import time
+import curses
+
+def print_menu(scr, selected, menu):
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLUE)
+    h, w = scr.getmaxyx();
+    for index, row in enumerate(menu):
+        x = w//2 - len(row) // 2
+        y = h//2 - len(menu) // 2 + index
+        if index == selected%len(menu) :
+            scr.attron(curses.color_pair(1))
+            scr.addstr(y, x, row)
+            scr.attroff(curses.color_pair(1))
+        else :
+            scr.addstr(y, x,row)
+    scr.refresh();
+
+def main(stdscr, menu):
+    curses.curs_set(0)
+    selection = 0;
+    while 1:
+        key = stdscr.getch() # get pressed key
+        stdscr.clear()
+        if key == curses.KEY_UP:
+            selection -= 1;
+        if key == curses.KEY_DOWN :
+            selection += 1;
+        if key in [10, 13] :
+            return selection%len(menu)
+            break;
+        stdscr.refresh();
+        print_menu(stdscr, selection, menu);
+
+def make_menu(menu):
+    return curses.wrapper(main, menu)
+
+if __name__ == '__main__' :
+    make_menu();
